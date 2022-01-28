e0c8e4c210684a145783832574271aaa2d0e3aa2
Venvolve <vnvolve@gmail.com>
2022-01-28 14:37:37 +0700

    update message notification

---
+++ js/message-functions.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.vn-message-open-add-file-button').click(function(){
+        $('#vn-message-open-add-file-content').toggleClass('active-content')
+        $('#vn-message-textbox-emoticon-content').removeClass('active-content')
+    })
+
+    $('#vn-emoticon-button').click(function(){
+        $('#vn-message-textbox-emoticon-content').toggleClass('active-content')
+        $('#vn-message-open-add-file-content').removeClass('active-content')
+    })
+
+    
+})
+
+function injectEmojisToList(e) {
+    document.getElementById("vn-message-textbox-text").value = e.innerHTML;
+}
