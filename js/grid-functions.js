a03faf3efd9d341b8df1d874b0b9e7f2e91b5235
Muhamad Syahrul Mubarok <12172949@bsi.ac.id>
2022-01-31 18:08:41 +0700

    Update

--- js/grid-functions.js
+++ js/grid-functions.js
@@ -1,24 +1,27 @@
 $(document).ready(function () {
-    $(".vn-item-love").click(function (e) {
-      e.preventDefault();
-      if ($(this).hasClass("love-color")) {
-        $(this).removeClass("love-color");
-      } else {
-        $(this).addClass("love-color");
-      }
-    });
+	$(".vn-item-love").click(function (e) {
+		e.preventDefault();
+		if ($(this).hasClass("love-color")) {
+			$(this).removeClass("love-color");
+		} else {
+			$(this).addClass("love-color");
+		}
+	});
 
-    $(".vn-item-bookmark").click(function (e) {
-        if ($(this).hasClass("bookmark-color")) {
-          $(this).removeClass("bookmark-color");
-        } else {
-          $(this).addClass("bookmark-color");
-        }
-      });
+	$(".vn-item-bookmark").click(function (e) {
+		if ($(this).hasClass("bookmark-color")) {
+			$(this).removeClass("bookmark-color");
+		} else {
+			$(this).addClass("bookmark-color");
+		}
+	});
 
-    $('#tes').click(function(e){
-        $(this.closest('.vn-info-3dot-button-content')).toggleClass('active')
-    })
-  });
-
-  
+	$(".vn-info-3dot-button").click(function (e) {
+		var active = $(this).next(".vn-info-3dot-button-content");
+		if (active.hasClass("active")) {
+			$(this).next(".vn-info-3dot-button-content").removeClass("active");
+		} else {
+			$(this).next(".vn-info-3dot-button-content").addClass("active");
+		}
+	});
+});
